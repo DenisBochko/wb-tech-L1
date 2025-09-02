f1501dd3057b4751a08e38b992e49bf37a5c51c3
reElcy <reElcy@yandex.ru>
2025-09-02 22:29:20 +0300

    feat: add phone to Human

--- L1_1/main.go
+++ L1_1/main.go
@@ -3,12 +3,13 @@ package main
 import "fmt"
 
 type Human struct {
-	name string
-	age  int
+	name  string
+	age   int
+	phone string
 }
 
 func (h Human) SayHi() {
-	fmt.Printf("Hi, I am %s you can call me on %s\n", h.name, h.name)
+	fmt.Printf("Hi, I am %s you can call me on %s\n", h.name, h.phone)
 }
 
 func (h Human) Sing(lyrics string) {
@@ -20,7 +21,7 @@ type Action struct {
 }
 
 func main() {
-	action := Action{Human{"James", 20}}
+	action := Action{Human{"James", 20, "+79876543211"}}
 
 	action.SayHi()
 	action.Sing("OOOOOOO...")
