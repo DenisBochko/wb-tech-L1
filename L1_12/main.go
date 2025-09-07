ba206446fba425be10614b1d740619dfcf250da3
reElcy <reElcy@yandex.ru>
2025-09-07 19:56:37 +0300

    feat: add resolve L1.12 task

---
+++ L1_12/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func uniqueSet(initialSet []string) []string {
+	set := make(map[string]bool)
+	resultSet := make([]string, 0, len(initialSet))
+
+	for _, word := range initialSet {
+		_, ok := set[word]
+		if !ok {
+			set[word] = true
+			resultSet = append(resultSet, word)
+		}
+	}
+
+	return resultSet
+}
+
+func main() {
+	words := []string{"cat", "cat", "dog", "cat", "tree"}
+
+	fmt.Println(uniqueSet(words))
+}
